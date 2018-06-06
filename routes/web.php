14ceb744b0f9eefe72fbfbd2365047a29a703502
nikochan <nikochan@gmail.com>
2018-06-06 07:17:45 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::get('/', 'MicropostsController@index');
+   Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
